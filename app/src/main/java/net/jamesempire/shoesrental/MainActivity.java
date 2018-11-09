a504cf3da373afa0fdeb7ba7df1493cd834ee667
Amazing <thienphuoc.0108@gmail.com>
2018-11-09 12:33:29 -0500

    Final ver

--- app/src/main/java/net/jamesempire/shoesrental/MainActivity.java
+++ app/src/main/java/net/jamesempire/shoesrental/MainActivity.java
@@ -17,23 +17,32 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final Integer[] shoesPic =  {R.drawable.adidas, R.drawable.jordan
+        final Integer[] shoesPic =  {R.drawable.adidas, R.drawable.jordan                           // Array of shoes image
                 , R.drawable.klay, R.drawable.lebron,
                 R.drawable.kobe,R.drawable.curry};
-        final String[] shoeName = {"Adidas Crazy Explosive" , "Air Jordan XX9", "Anta KT3" ,"Lebron 15" , "Nike Zoom Kobe 1 protro","Under Armour Curry 2"};
-        final Integer[] shoeRentalPrice = {160, 185, 120, 185, 175, 135} ;
+        final String[] shoeName = {"Adidas Crazy Explosive" , "Air Jordan XX9", "Anta KT3" ,"Lebron 15" , "Nike Zoom Kobe 1 protro","Under Armour Curry 2"};  //Array of shoes name appropriate with the images
+        final Integer[] shoeRentalPrice = {160, 185, 120, 185, 175, 135} ;                          //Array of shoes rental price appropriate with the images
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Display the Action Bar Icon
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        //Locate a variable with an object on the layout
         GridView grid = findViewById(R.id.gridView);
         final ImageView rentedShoeImage = findViewById(R.id.imgRented);
 
         //Declare an output format for the output
         final DecimalFormat currency = new DecimalFormat("###$");
 
+        //Image Adapter bind the data model to the grid view
         grid.setAdapter(new ImageAdapter(this));
+
+        //A large image indicated bellow the grid view when the user click an item on the grid view
         grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -44,12 +53,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
     public class ImageAdapter extends BaseAdapter
     {
-        final Integer[] shoesPic =  {R.drawable.adidas, R.drawable.jordan
+        final Integer[] shoesPic =  {R.drawable.adidas, R.drawable.jordan                            // Array of shoes image
                 , R.drawable.klay, R.drawable.lebron,
                 R.drawable.kobe,R.drawable.curry};
 
+        //Hold the image temporary in the grid view
         private Context context;
         public ImageAdapter(Context c) {
             context = c;
@@ -71,11 +82,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        //Set up the image in the grid view
         public View getView(int position, View convertView, ViewGroup parent) {
             ImageView gridViewShoeImage = new ImageView(context);
             gridViewShoeImage.setImageResource(shoesPic[position]);
             gridViewShoeImage.setScaleType(ImageView.ScaleType.FIT_XY);
             gridViewShoeImage.setLayoutParams(new GridView.LayoutParams(330,300));
+            gridViewShoeImage.setBackgroundResource(R.drawable.stroke);
             return gridViewShoeImage;
         }
     }
